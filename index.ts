7fd69435173dab92bc9d08d7488e274797da0166
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2025-02-21 15:33:14 +0500

    .

---
+++ index.ts
@@ -0,0 +1,54 @@
+//1-mashq
+// let num: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function kv(n: number[]): number[] {
+//     return n.map((value) => {
+//       return value * value;
+//   })
+// }
+
+// console.log(kv(num));
+
+
+//2-mashq
+// let str: string[] = [
+//     'abdulaziz',
+//     'abdurahimov',
+//     'olma',
+//     'anor',
+//     'uzum'
+// ];
+
+// function res(n: string[], minLength: number): string[] {
+//     return n.filter((value) => {
+//         return value.length >= minLength;
+//     });
+// }
+
+// console.log(res(str, 5));  buni qila olmadim
+
+//3-mashq
+// let str = prompt("ismingizni kiriting");
+// let arr: string[] = ["abdulaziz", "abdurahimov", "test"];
+
+// function res(n: string[]): string {
+//   return n.includes(str as string) ? "malumot bor" : "malumot yoq";
+// }
+
+// console.log(res(arr));
+
+
+//4-mashq
+// let num: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// function res(n:number[]):number[] {
+//     return n.filter((value) => {
+//         if (value%2==0) {
+//             return value
+//         } 
+//     })
+// }
+// console.log(res(num));
+
+
+
+
